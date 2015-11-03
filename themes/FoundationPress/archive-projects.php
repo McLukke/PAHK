eb74576c892c20e693d53c20bb7fdb9daecc2cc0
Luke McKennan <luke.mckennan@gmail.com>
2015-11-03 11:34:11 +0800

    do shortcode doesn't work....

--- themes/FoundationPress/archive-projects.php
+++ themes/FoundationPress/archive-projects.php
@@ -19,10 +19,7 @@
 get_header();
 
 if ( "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] == get_home_url() || "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] == get_home_url()."/" ) {
-	// paste carousel here
-	// echo "CAROUSEL";
 	putRevSlider("slider1", "homepage");
-	echo do_shortcode([rev_slider alias="slider1"]);
 }
 
 require_once('templates/list_projects.php');
