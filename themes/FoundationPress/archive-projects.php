769abed4fd5a46b8f161dad5bd1bfcb3349e4ba0
Luke McKennan <luke.mckennan@gmail.com>
2015-11-03 13:08:32 +0800

    wip

--- themes/FoundationPress/archive-projects.php
+++ themes/FoundationPress/archive-projects.php
@@ -19,8 +19,8 @@
 get_header();
 
 if ( "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] == get_home_url() || "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] == get_home_url()."/" ) {
-	echo "CAROUSEL";
 	putRevSlider("slider1");
+	
 }
 
 require_once('templates/list_projects.php');
