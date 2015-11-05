31e6614f009ae3d1d450d32171551c0b7010ab70
Luke McKennan <luke.mckennan@gmail.com>
2015-11-05 12:27:22 +0800

    removing infinite scroll plugin, doesn't work

--- themes/FoundationPress/templates/list_projects.php
+++ themes/FoundationPress/templates/list_projects.php
@@ -46,10 +46,12 @@ while ( have_posts() ) : the_post();
     } ?></h5>
     <h6><?php 
       if (qtranxf_getLanguage() === "zh") {
-        echo date_i18n( 'M', strtotime(pods_field_display ('projects', get_the_ID(), 'display_from')) )."月 ".date_i18n( 'Y', strtotime(pods_field_display ('projects', get_the_ID(), 'display_from')) );
+        // echo date_i18n( 'M', strtotime(pods_field_display ('projects', get_the_ID(), 'display_from')) )."月 "
+        //.display_chinese_date(date_i18n( 'Y', strtotime(pods_field_display ('projects', get_the_ID(), 'display_from')) ));
+        // .date_i18n( 'Y', strtotime(pods_field_display ('projects', get_the_ID(), 'display_from')) );
       } else {
-        echo date_i18n( 'M Y', strtotime( pods_field_display ('projects', get_the_ID(), 'display_from')) );
-      } ?> - <?php
+        echo date_i18n( 'M Y', strtotime( pods_field_display ('projects', get_the_ID(), 'display_from')) ); 
+      } ?> - <?php 
       if (qtranxf_getLanguage() === "zh") {
         echo date_i18n( 'M', strtotime(pods_field_display ('projects', get_the_ID(), 'display_until')) )."月 ".date_i18n( 'Y', strtotime(pods_field_display ('projects', get_the_ID(), 'display_until')) );
       } else {
