60792e0ea000d04affd16ffdcf7d8d4743705087
Luke McKennan <luke.mckennan@gmail.com>
2015-11-05 13:03:29 +0800

    Revert "removing infinite scroll plugin, doesn't work"
    This reverts commit 31e6614f009ae3d1d450d32171551c0b7010ab70.

--- themes/FoundationPress/functions.php
+++ themes/FoundationPress/functions.php
@@ -48,47 +48,27 @@ require_once( 'library/custom-nav.php' );
 /** Add protocol relative theme assets */
 require_once( 'library/protocol-relative-theme-assets.php' );
 
-// function display_chinese_year ($input_year) {
-//     $year_array = str_split($input_year);
-//     $year_zh = "";
-//     $temp = "";
-//     foreach ($year_array as $number) {
-//         switch ($number) {
-//             case "0":
-//                 $year_zh = $year_zh."零";
-//                 break;
-//             case "1":
-//                 $year_zh = $year_zh."一";
-//                 break;
-//             case "2":
-//                 $year_zh = $year_zh."二";
-//                 break;
-//             case "3":
-//                 $year_zh = $year_zh."三";
-//                 break;
-//             case "4":
-//                 $year_zh = $year_zh."四";
-//                 break;
-//             case "5":
-//                 $year_zh = $year_zh."五";
-//                 break;
-//             case "6":
-//                 $year_zh = $year_zh."六";
-//                 break;
-//             case "7":
-//                 $year_zh = $year_zh."七";
-//                 break;
-//             case "8":
-//                 $year_zh = $year_zh."八";
-//                 break;
-//             case "9":
-//                 $year_zh = $year_zh."九";
-//                 break;
-//         }
-//     }
-//     return $year_zh;
-// }
+add_action("pre_get_posts", "custom_front_page");
+function custom_front_page($wp_query){
+    //Ensure this filter isn't applied to the admin area
+    if(is_admin()) {
+        return;
+    }
 
+    if($wp_query->get('page_id') == get_option('page_on_front')):
 
+        $wp_query->set('post_type', 'projects');
+        $wp_query->set('page_id', ''); //Empty
+
+        //Set properties that describe the page to reflect that
+        //we aren't really displaying a static page
+        $wp_query->is_page = 0;
+        $wp_query->is_singular = 0;
+        $wp_query->is_post_type_archive = 1;
+        $wp_query->is_archive = 1;
+
+    endif;
+
+}
 
 ?>
